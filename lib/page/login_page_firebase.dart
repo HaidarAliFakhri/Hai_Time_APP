104582c44b4f8bb7c0146a7a26c85a96de4abd57
HaidarAliFakhri <haidar.ali.fakhri.999@gmail.com>
2025-11-24 14:56:41 +0700

    update

---
+++ lib/page/login_page_firebase.dart
@@ -0,0 +1,319 @@
+import 'package:flutter/material.dart';
+import 'package:hai_time_app/page/bottom_navigator.dart';
+import 'package:hai_time_app/page/form_register.dart';
+import 'package:hai_time_app/page/home_page.dart';
+import 'package:hai_time_app/preferences/preferences_handler.dart';
+import 'package:hai_time_app/view/dashborad_admin.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LoginPageFireBase extends StatefulWidget {
+  const LoginPageFireBase({super.key});
+  static const id = "/HaiTime";
+
+  @override
+  State<LoginPageFireBase> createState() => _LoginPageFireBaseState();
+}
+
+class _LoginPageFireBaseState extends State<LoginPageFireBase> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  bool _isHidden = true;
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _login() async {
+    if (!_formKey.currentState!.validate()) return;
+
+    final prefs = await SharedPreferences.getInstance();
+
+    final registeredEmail = prefs.getString('registered_email');
+    final registeredPassword = prefs.getString('registered_password');
+
+    final emailInput = emailController.text.trim();
+    final passwordInput = passwordController.text.trim();
+
+    //  1. Login Admin
+    if (emailInput == 'admin@haitime.com' && passwordInput == 'admin123') {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Login sebagai Admin berhasil!')),
+      );
+
+      //  Simpan status login admin
+      await PreferenceHandler.saveLogin(true);
+      await PreferenceHandler.setEmail(emailInput);
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const DashboardAdminPage()),
+      );
+      return;
+    }
+
+    //  2. Login User
+    if (emailInput == registeredEmail && passwordInput == registeredPassword) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Login berhasil! Selamat datang 👋')),
+      );
+
+      //  Simpan status login user
+      await PreferenceHandler.saveLogin(true);
+      await PreferenceHandler.setEmail(emailInput);
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => BottomNavigator()),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Email atau password salah')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/background/background.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(color: Colors.black.withOpacity(0.5)),
+
+          // Form Login
+          Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.cloud, size: 80, color: Colors.white),
+                    const SizedBox(height: 16),
+                    const Text(
+                      "HaiTime",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.5,
+                      ),
+                    ),
+                    const SizedBox(height: 32),
+
+                    // Email
+                    TextFormField(
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        hintText: "Email",
+                        filled: true,
+                        fillColor: Colors.white.withOpacity(0.85),
+                        prefixIcon: const Icon(Icons.email),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Email wajib diisi";
+                        }
+                        if (!value.contains("@")) {
+                          return "Email tidak valid";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16),
+
+                    // Password
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: _isHidden,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        filled: true,
+                        fillColor: Colors.white.withOpacity(0.85),
+                        prefixIcon: const Icon(Icons.lock),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _isHidden ? Icons.visibility_off : Icons.visibility,
+                          ),
+                          onPressed: () =>
+                              setState(() => _isHidden = !_isHidden),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Password wajib diisi";
+                        }
+                        if (value.length < 6) {
+                          return "Minimal 6 karakter";
+                        }
+                        return null;
+                      },
+                    ),
+
+                    const SizedBox(height: 28),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const HomePage(),
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            "Forget Password",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 28),
+                    // Tombol Login
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF2F4B7A),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                        onPressed: _login,
+                        child: const Text(
+                          "Masuk",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(height: 24),
+
+                    // Pembatas
+                    Row(
+                      children: const [
+                        Expanded(
+                          child: Divider(
+                            thickness: 1,
+                            indent: 20,
+                            endIndent: 10,
+                            color: Colors.white70,
+                          ),
+                        ),
+                        Text(
+                          "Or continue with",
+                          style: TextStyle(color: Colors.white70, fontSize: 14),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            thickness: 1,
+                            indent: 10,
+                            endIndent: 20,
+                            color: Colors.white70,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16),
+
+                    // Tombol Sosial Media
+                    // Row(
+                    //   mainAxisAlignment: MainAxisAlignment.center,
+                    //   children: [
+                    //     _socialButton("assets/images/icon/google.jpg"),
+                    //     const SizedBox(width: 20),
+                    //     _socialButton("assets/images/icon/apple.jpg"),
+                    //     const SizedBox(width: 20),
+                    //     _socialButton("assets/images/icon/twitter.jpg"),
+                    //   ],
+                    // ),
+                    const SizedBox(height: 20),
+
+                    // Register
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Belum punya akun? ",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const RegisterPage(),
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            "Register",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Tombol sosial media
+// Widget _socialButton(String assetPath) {
+//   return Container(
+//     padding: const EdgeInsets.all(10),
+//     decoration: BoxDecoration(
+//       color: Colors.white,
+//       borderRadius: BorderRadius.circular(12),
+//       boxShadow: [
+//         BoxShadow(
+//           color: Colors.black.withOpacity(0.1),
+//           blurRadius: 5,
+//           offset: const Offset(0, 2),
+//         ),
+//       ],
+//     ),
+//     child: Image.asset(assetPath, width: 30, height: 30),
+//   );
+// }
